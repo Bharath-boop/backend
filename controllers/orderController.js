f606c54dd383092dd7e355fb0b2e1bc6f6fba336
Bharath kumar <bharathboopathi51550@gmail.com>
2024-06-14 19:25:01 +0530

    frent-end

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from 'stripe'
 const stripe = new Stripe(process.env.SCRIPE_SECRET_KEY)
 //place order
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://orange-roan.vercel.app/"
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
